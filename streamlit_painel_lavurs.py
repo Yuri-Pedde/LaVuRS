476a98fa250b08a0ed3126a7fc243f96806b4acf
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-25 19:43:16 -0300

    Update streamlit_painel_lavurs.py

--- streamlit_painel_lavurs.py
+++ streamlit_painel_lavurs.py
@@ -912,7 +912,7 @@ if page=="Painel LaVuRS":
             titleFontSize=18,
             titleColor='black',
             labelFontSize=12,
-        ).properties(height=400,width=1700)
+        ).properties(height=400,width=1680)
         # Gráfico com título
         chart_with_title = layout_chart.properties(title=title_properties)
 
