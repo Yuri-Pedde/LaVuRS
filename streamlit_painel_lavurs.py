72b84d603317de7e36ede3e82eb1a6f64011bbf9
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-25 18:35:16 -0300

    Update streamlit_painel_lavurs.py

--- streamlit_painel_lavurs.py
+++ streamlit_painel_lavurs.py
@@ -18,7 +18,7 @@ st.set_page_config(
     page_title="LaVuRS",
     page_icon="🌍",
     layout="wide",
-    initial_sidebar_state='auto'
+    initial_sidebar_state='expanded'
 )
 
 container1 = st.container()
