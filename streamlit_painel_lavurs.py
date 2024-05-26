1c46f5bc355ea0af1168001adde363fd6e24698a
Yuri Georg Pedde <108555947+Yuri-Pedde@users.noreply.github.com>
2024-05-26 10:05:45 -0300

    Update streamlit_painel_lavurs.py

--- streamlit_painel_lavurs.py
+++ streamlit_painel_lavurs.py
@@ -151,7 +151,20 @@ if page=="Painel LaVuRS":
         #st.write('<div style="margin-top: 15px;">', unsafe_allow_html=True)
         #st.write('</div>', unsafe_allow_html=True)
         coluna1,coluna1_5, coluna2, coluna2_5, coluna3, coluna3_5, coluna4, coluna4_5, coluna5 = st.columns([1,0.1,1,0.1,1,0.1,1,0.1,1])
-    
+        
+        st.markdown("""
+        <style>
+        #root > div:nth-child(1) > div.withScreencast > div > div > div > section.main.st-emotion-cache-bm2z3a.ea3mdgi8 > div.block-container.st-emotion-cache-1jicfl2.ea3mdgi5 > div > div > div > div:nth-child(6) > div > div
+        {
+            background: linear-gradient(to bottom, #66b366 0%, #66b366 5%, #80cc80 10%, #99e599 20%, #b3e5c0 30%, white 100%);
+            box-shadow: 0px 0px 5px 5px rgba(0, 0, 0, 0.25);
+            padding: 20px;
+            border-radius: 15px;
+            border: 3px solid green;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+        
         with coluna1:
             
             lista_decadas_todas_filtro = list(df_original['Década'].unique())
